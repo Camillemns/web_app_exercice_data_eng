6f5880b790f0ebe52752f014982dae19033ee88f
Camille <camille.emma@gmail.com>
2022-01-30 11:38:47 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Web App Exercice with docker and jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
